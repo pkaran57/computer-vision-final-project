3e9ee2c0ec25a4c82ece9a66728f781400dd97af
Karan Patel <karanpatel4567@gmail.com>
2021-06-09 09:36:53 -0700

    Refactoring

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ from src.definitions import OUTPUT_DIR
 tf.get_logger().setLevel("ERROR")
 
 
-def load_tf_hub_model():
+def load_tf_hub_model(model_name):
     print("loading model {} ...".format(model_name))
     module_handle = ALL_MODELS[model_name]
     hub_model = hub.load(module_handle)
@@ -21,42 +21,46 @@ def load_tf_hub_model():
     return hub_model
 
 
-if __name__ == '__main__':
+def get_image_with_predictions(original_image, result):
+    image_with_predictions = original_image.numpy().copy()
+
+    viz_utils.visualize_boxes_and_labels_on_image_array(
+        image_with_predictions,
+        result["detection_boxes"][0].numpy(),
+        result["detection_classes"][0].numpy().astype(int),
+        result["detection_scores"][0].numpy(),
+        category_index,
+        use_normalized_coordinates=True,
+        max_boxes_to_draw=200,
+        min_score_thresh=0.30,
+        agnostic_mode=False,
+    )
+
+    return image_with_predictions
+
+
+if __name__ == "__main__":
 
     category_index = get_category_index()
     coco_dataset = load_dataset()
 
     for model_name in ["Faster R-CNN Inception ResNet V2 1024x1024"]:
-
-        hub_model = load_tf_hub_model()
+        hub_model = load_tf_hub_model(model_name)
 
         for sample in coco_dataset:
-            captions = sample["captions"]
-            image = sample["image"]
-            image_file_name = sample["image/filename"]
-            image_id = sample["image/id"]
-            objects = sample["objects"]
-
-            cv2.imwrite(os.path.join(OUTPUT_DIR, "{}-original.png".format(model_name)), image.numpy())
-
-            result = hub_model(tf.expand_dims(image, axis=0))
-
-            image_np_with_detections = image.numpy().copy()
-
-            viz_utils.visualize_boxes_and_labels_on_image_array(
-                image_np_with_detections,
-                result["detection_boxes"][0].numpy(),
-                result["detection_classes"][0].numpy().astype(int),
-                result["detection_scores"][0].numpy(),
-                category_index,
-                use_normalized_coordinates=True,
-                max_boxes_to_draw=200,
-                min_score_thresh=0.30,
-                agnostic_mode=False,
+            original_image = sample["image"]
+            cv2.imwrite(
+                os.path.join(OUTPUT_DIR, "{}-original.png".format(model_name)),
+                original_image.numpy(),
             )
 
+            result = hub_model(tf.expand_dims(original_image, axis=0))
+
+            image_with_predictions = get_image_with_predictions(original_image, result)
+
             cv2.imwrite(
-                os.path.join(OUTPUT_DIR, "{}-output-image.png".format(model_name)), image_np_with_detections
+                os.path.join(OUTPUT_DIR, "{}-output-image.png".format(model_name)),
+                image_with_predictions,
             )
 
             break
